88dc72cd87cf203b9f82f97d2b29c5657f880085
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-06-20 14:44:27 -0300

    somaLinhas C#

---
+++ 3- C#/Exercícios/5- Matrizes/SomaLinhas/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int M, N, i, j;
+
+Console.Write("Digite a quantidade de linhas que deseja: ");
+M = int.Parse(Console.ReadLine());
+
+Console.Write("Digite a quantidade de colunas que deseja: ");
+N = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[M, N];
+int[] totais = new int[M];
+
+for (i = 0; i < M; i++)
+{
+    for (j = 0; j < N; j++)
+    {
+        Console.Write($"Elemento [{i},{j}]");
+        matriz[i,j] = int.Parse(Console.ReadLine());
+        totais[i] = totais[i] + matriz[i,j];
+    }
+
+}
+
+Console.WriteLine();
+
+for (i = 0; i < M; i++)
+{
+    Console.Write($"Total linha {i}: " + totais[i]);
+    Console.WriteLine();
+}
