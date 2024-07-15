783e72ac2bd1b9e37ac0a55406e63a06aeebb949
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-07-15 17:58:49 -0300

    mais javascript

---
+++ 3- Javascript/Ex3- Estrutura condicional/01iniciandoEstruturaCondicional.js
@@ -0,0 +1,12 @@
+console.log('O Console funcionou corretamente!')
+
+var vel = 60
+
+if (vel > 60) {
+    console.log(`A velocidade do seu carro era de ${vel}km/h, você será multado!` )
+}
+else{ 
+    console.log(`A velocidade do seu carro era de ${vel}km/h, você está dentro da velocidade da via! Parabéns!` )
+}
+
+
