f5a233486ac163f3718e78a78923ed0d317b0a4a
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-07-15 19:12:31 -0300

    Create 04exVotacao.js

---
+++ 3- Javascript/Ex3- Estrutura condicional/04exVotacao.js
@@ -0,0 +1,9 @@
+var idade = 18
+
+if (idade < 16){
+    console.log('Você não pode votar!')
+} else if (idade < 18) {
+    console.log('Voto facultativo!')
+} else {
+    console.log('Voto obrigatório!')
+}
