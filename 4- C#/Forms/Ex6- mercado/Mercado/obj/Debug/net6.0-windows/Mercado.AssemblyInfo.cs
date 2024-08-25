cef792446511e24b5547a6ec8ccc9ab0db8ef5a9
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-08-25 19:50:46 -0300

    .

--- 4- C#/Forms/Ex6- mercado/Mercado/obj/Debug/net6.0-windows/Mercado.AssemblyInfo.cs
+++ 4- C#/Forms/Ex6- mercado/Mercado/obj/Debug/net6.0-windows/Mercado.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mercado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527824fdad20d6b08cae9eee372ea429145a6694")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mercado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mercado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
