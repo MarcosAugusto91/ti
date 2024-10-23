6ca50ff4008c964f12252b2c8a43bff579498456
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-10-23 00:43:50 -0300

    Atualização Mosquito

--- 3- Javascript/Ex14- Mosquito/scripts/jogo.js
+++ 3- Javascript/Ex14- Mosquito/scripts/jogo.js
@@ -10,8 +10,13 @@ ajustaTamanhoTela()
 
 function randomizaPosicao(){
 
-    let posicaoX = Math.floor(Math.random() * largura) -50
-    let posicaoY = Math.floor(Math.random() * altura) -50
+    //remover mosquito anterior
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove() 
+    }
+
+    let posicaoX = Math.floor(Math.random() * largura) -100
+    let posicaoY = Math.floor(Math.random() * altura) -100
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX 
     posicaoY = posicaoY < 0 ? 0 : posicaoY 
@@ -21,18 +26,19 @@ function randomizaPosicao(){
     //criar o elemento HTML
     let mosquito = document.createElement('img')
     mosquito.src = 'imagens/mosca.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = (tamanhoAleatorio() + ' ' +  ladoAleatorio())
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
-    
+    mosquito.id = 'mosquito'
+
+    //colocar o elemento na tela
     document.body.appendChild(mosquito)
-    console.log(tamanhoAleatorio())
 }
 
+//Escolher o tamanho do mosquito
 function tamanhoAleatorio(){
     let classe = Math.floor(Math.random() * 3)
-    console.log(classe)
 
     switch(classe){
         case 0:
@@ -41,5 +47,17 @@ function tamanhoAleatorio(){
             return "mosquito2"
         case 2: 
             return "mosquito3"
+    }
+}
+
+//Escolher a direção do mosquito
+function ladoAleatorio(){
+    let lado = Math.floor(Math.random() * 2)
+
+    switch(lado){
+        case 0:
+            return "ladoA"
+        case 1:
+            return "ladoB"
     }
 }
