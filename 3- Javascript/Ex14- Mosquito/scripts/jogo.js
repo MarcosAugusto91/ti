0c63e1f68695590c05ba4fac085c56ce10a9f457
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-10-23 16:18:37 -0300

    Projeto mosquito finalizado

--- 3- Javascript/Ex14- Mosquito/scripts/jogo.js
+++ 3- Javascript/Ex14- Mosquito/scripts/jogo.js
@@ -1,32 +1,48 @@
 let altura, largura = 0
 let vidas = 1
+let temporizador = 1500
+ajustaTamanhoTela()
 
+//Seleciona o tempo de acordo com o nivel
+let nivel = window.location.search
+    nivel = nivel.replace('?', '')
+    
+    if (nivel == 'facil'){
+            temporizador = 1500
+        } else if (nivel == 'normal'){
+            temporizador = 1000
+        } else {
+            temporizador = 800
+        }
+
+
+//Ajustando tamanho da tela
 function ajustaTamanhoTela() {
     altura = window.innerHeight
     largura = window.innerWidth
     console.log("Largura:", largura, "Altura:", altura)
 }
 
-ajustaTamanhoTela()
 
-let cronometro = setInterval(
-    function(){
+//Contagem do tempo e fluxo da vitória
+let cronometro = setInterval( function(){
             tempo -= 1
                 if( tempo < 0){
                     clearInterval(cronometro)
                     clearInterval(criaMosquito)
                     window.location.href = 'vitoria.html'
                 } else{ 
-                document.getElementById('cronometro').innerHTML = tempo
+                    document.getElementById('cronometro').innerHTML = tempo
                 }
             }, 1000)
 
-function randomizaPosicao(){
 
+function randomizaPosicao(){
     //remover mosquito anterior
     if(document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove() 
 
+        //diminuindo a quantidade de vidas e criando o fim do jogo
         if(vidas > 3){
             window.location.href = 'fim_de_jogo.html'}
             else{
@@ -35,6 +51,7 @@ function randomizaPosicao(){
             }
         }
 
+    //randomizando posição na tela
     let posicaoX = Math.floor(Math.random() * largura) -100
     let posicaoY = Math.floor(Math.random() * altura) -100
 
@@ -58,6 +75,7 @@ function randomizaPosicao(){
     document.body.appendChild(mosquito)
 }
 
+
 //Escolher o tamanho do mosquito
 function tamanhoAleatorio(){
     let classe = Math.floor(Math.random() * 3)
@@ -72,6 +90,7 @@ function tamanhoAleatorio(){
     }
 }
 
+
 //Escolher a direção do mosquito
 function ladoAleatorio(){
     let lado = Math.floor(Math.random() * 2)
@@ -84,12 +103,20 @@ function ladoAleatorio(){
     }
 }
 
+
 //reiniciando o jogo
 function reiniciar(){
     window.location.href = 'index.html'
 }
 
+
 //iniciando o jogo
 function iniciar(){
-    window.location.href = 'app.html'
+    let nivel = document.getElementById('combo').value
+    if(nivel == 0){
+        alert('Obrigatório a seleção de um nível!')
+    } else{
+       // window.alert(nivel)
+        window.location.href = 'app.html' + '?' + nivel
+    }
 }
