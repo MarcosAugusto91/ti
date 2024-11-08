91e862961288dfa0f4f3cc7fd2357c065154c939
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-11-08 10:39:56 -0300

    Create array.php

---
+++ 6- Php/php_estruturada/array.php
@@ -0,0 +1,27 @@
+<?php
+
+// $frutas = array('banana','maçã','morango');
+
+// $frutas = [];
+
+ $frutas = ['b' => 'banana', 'm' => 'maçã', 'f' => 'morango'];
+
+ echo '<pre>';
+ var_dump($frutas);
+ echo '</pre>';
+
+ echo '<pre>';
+ print_r($frutas);
+ echo '</pre>';
+
+ echo $frutas['b'];
+
+ $_cadastroAlunos['nomes'] = ['Marcos', 'Leticia', 'Daniel', ' Gaby', 'Lucas'];
+ $_cadastroAlunos['idade'] = [33,16,33,20,18];
+
+ echo '<pre>';
+ var_dump($_cadastroAlunos);
+ echo '</pre>';
+
+
+?>
