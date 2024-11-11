9bc398cb61eacf873acfb536fe937993ff42790b
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2024-11-11 08:04:27 -0300

    Create foreach.php

---
+++ 6- Php/php_estruturada/foreach.php
@@ -0,0 +1,13 @@
+<?php
+    //Inserindo o Array
+    $listadefilmes = [
+            'Piratas do caribe',
+            'Edward mãos de tesoura',
+            'A Fantástica fábrica de chocolate',
+            'Vingadores',
+            'Mario', ];
+
+    foreach($listadefilmes as $filmes){
+        echo "$filmes <br/>";
+    }
+?>
