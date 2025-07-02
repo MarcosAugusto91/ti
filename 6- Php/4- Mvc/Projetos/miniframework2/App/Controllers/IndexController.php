9e3e80e5706f4ec40f1c1a1c1f070d8de54693c9
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2025-07-02 14:05:29 -0300

    .

---
+++ 6- Php/4- Mvc/Projetos/miniframework2/App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController {
+
+	public function index() {
+		echo 'Chegamos ao IndexController e disparamentos a action index';
+	}
+
+	public function sobreNos() {
+		echo 'Chegamos ao IndexController e disparamentos a action sobreNos';
+	}
+}
+
+
+?>
