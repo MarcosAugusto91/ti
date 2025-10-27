e3ed6054582b095cac928ddf15bb3950304d56fa
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2025-10-27 17:38:14 -0300

    .

--- 3- Javascript/Ex06- Ex. Extra/Desafio 2/script.js
+++ 3- Javascript/Ex06- Ex. Extra/Desafio 2/script.js
@@ -1,32 +1,199 @@
+let nivel, valortentativa, formulario, formjogo, resultado, valornivel, maximo, tentativas, numeroAleatorio;
+
+let contador = 1;
+
+nivel = window.document.getElementById('nivel');
+btniniciar = window.document.getElementById('btniniciar');
+nivel.focus();
+
+valortentativa = window.document.getElementById('valortentativa');
+btnjogar = window.document.getElementById('btnconfirmar');
+
+nivel.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // evita comportamento padrão
+        btniniciar.click(); // simula clique no botão
+    }
+});
+
+valortentativa.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // evita comportamento padrão
+        btnjogar.click(); // simula clique no botão
+    }
+});
+
+
+//---------------------------------- Inicia o jogo
 function iniciar() {
-    let nivel = window.document.getElementById('nivel');
-    let valortentativa = window.document.getElementById('valortentativa');
-    let formulario = window.document.getElementById('formnivel');
-    let formjogo = window.document.getElementById('formjogo')
-    let resultado = window.document.getElementById('resultado');
-    let valornivel = Number(nivel.value);
-    let maximo, tentativas;
+    formulario = window.document.getElementById('formnivel');
+    formjogo = window.document.getElementById('formjogo')
+    resultado = window.document.getElementById('resultado');
+    valornivel = Number(nivel.value);
 
-    formulario.style.display = 'none';
-    formjogo.style.display = 'flex';
-
-    if (valornivel == 1){
+    if (valornivel == 1) {
         maximo = 10;
         tentativas = 3;
-        valortentativa.setAttribute('max',`${maximo}`)
+        valortentativa.setAttribute('max', `${maximo}`)
     } else if (valornivel == 2) {
         maximo = 100;
         tentativas = 10;
-        valortentativa.setAttribute('max',`${maximo}`)
+        valortentativa.setAttribute('max', `${maximo}`)
         valortentativa.style.width = '60px';
     } else if (valornivel == 3) {
         maximo = 1000;
         tentativas = 15;
-        valortentativa.setAttribute('max',`${maximo}`);      
+        valortentativa.setAttribute('max', `${maximo}`);
         valortentativa.style.width = '70px';
     } else {
         resultado.innerHTML = "Digite um valor válido (1 à 3)!"
-     }
+        nivel.value = '';
+        return;
+    }
 
-    resultado.innerHTML = `Máximo: ${maximo} - Tentativas: ${tentativas}`;
+    formulario.style.display = 'none';
+    formjogo.style.display = 'flex';
+    resultado.innerHTML = `Você tem ${tentativas} tentativas para acertar o número entre 1 e ${maximo}. Boa sorte!`;
+    valortentativa.focus();
+
+    if (valornivel == 1) {
+        // Gera um número aleatório entre 1 e 10
+        numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+    } else if (valornivel == 2) {  
+        // Gera um número aleatório entre 1 e 100
+        numeroAleatorio = Math.floor(Math.random() * 100) + 1;      
+    } else if (valornivel == 3) {
+        // Gera um número aleatório entre 1 e 1000
+        numeroAleatorio = Math.floor(Math.random() * 1000) + 1;     
+    }
 }
+
+    //-----------------------------------Função jogar
+
+    function jogar() {
+
+        valortentativa = window.document.getElementById('valortentativa');
+        let valorjogado = Number(valortentativa.value);
+
+        switch (valornivel) {
+            case 1:
+                // Validando o valor jogado
+                if (valorjogado < 1 || valorjogado > 10) {
+                    alert("Digite um valor válido entre 1 e 10!");
+                    return;
+                }
+                // Verificando se o usuário acertou
+                else if (valorjogado === numeroAleatorio) {
+                    resultado.innerHTML = `Parabéns! Você acertou com ${contador} tentativas!`;
+                    btnjogar.value = "Jogar Novamente";
+                    btnjogar.onclick = function () { location.reload() };
+                    btnjogar.style.width = '120px';
+                    return;
+                }
+                else {
+                    // Dando dica ao usuário
+                    if (valorjogado < numeroAleatorio) {
+                        resultado.innerHTML = "Você errou! Dica: O número é maior!";
+                    } else {
+                        resultado.innerHTML = "Você errou! Dica: O número é menor!";
+                    }
+                    // Verificando tentativas restantes
+                    if (tentativas === 1) {
+                        resultado.innerHTML = `Suas tentativas acabaram! O número era ${numeroAleatorio}.`;
+                        btnjogar.value = "Jogar Novamente";
+                        btnjogar.onclick = function () { location.reload() };
+                        btnjogar.style.width = '120px';
+                        btnjogar.focus();
+                        return;
+                    }
+
+                    // Reduzindo tentativas
+                    tentativas--;
+                    btnjogar.value = `${contador + 1}º Tentativa`;
+                    contador++;
+                    valortentativa.value = '';
+                    valortentativa.focus();
+                }
+                break;
+
+            case 2:
+                // Validando o valor jogado
+                if (valorjogado < 1 || valorjogado > 100) {
+                    alert("Digite um valor válido entre 1 e 10!");
+                    return;
+                }
+                // Verificando se o usuário acertou
+                else if (valorjogado === numeroAleatorio) {
+                    resultado.innerHTML = `Parabéns! Você acertou com ${contador} tentativas!`;
+                    btnjogar.value = "Jogar Novamente";
+                    btnjogar.onclick = function () { location.reload() };
+                    btnjogar.style.width = '120px';
+                    return;
+                }
+                else {
+                    // Dando dica ao usuário
+                    if (valorjogado < numeroAleatorio) {
+                        resultado.innerHTML = "Você errou! Dica: O número é maior!";
+                    } else {
+                        resultado.innerHTML = "Você errou! Dica: O número é menor!";
+                    }
+                    // Verificando tentativas restantes
+                    if (tentativas === 1) {
+                        resultado.innerHTML = `Suas tentativas acabaram! O número era ${numeroAleatorio}.`;
+                        btnjogar.value = "Jogar Novamente";
+                        btnjogar.onclick = function () { location.reload() };
+                        btnjogar.style.width = '120px';
+                        btnjogar.focus();
+                        return;
+                    }
+
+                    // Reduzindo tentativas
+                    tentativas--;
+                    btnjogar.value = `${contador + 1}º Tentativa`;
+                    contador++;
+                    valortentativa.value = '';
+                    valortentativa.focus();
+                }
+                break;
+
+            case 3:
+                // Validando o valor jogado
+                if (valorjogado < 1 || valorjogado > 1000) {
+                    alert("Digite um valor válido entre 1 e 10!");
+                    return;
+                }
+                // Verificando se o usuário acertou
+                else if (valorjogado === numeroAleatorio) {
+                    resultado.innerHTML = `Parabéns! Você acertou com ${contador} tentativas!`;
+                    btnjogar.value = "Jogar Novamente";
+                    btnjogar.onclick = function () { location.reload() };
+                    btnjogar.style.width = '120px';
+                    return;
+                }
+                else {
+                    // Dando dica ao usuário
+                    if (valorjogado < numeroAleatorio) {
+                        resultado.innerHTML = "Você errou! Dica: O número é maior!";
+                    } else {
+                        resultado.innerHTML = "Você errou! Dica: O número é menor!";
+                    }
+                    // Verificando tentativas restantes
+                    if (tentativas === 1) {
+                        resultado.innerHTML = `Suas tentativas acabaram! O número era ${numeroAleatorio}.`;
+                        btnjogar.value = "Jogar Novamente";
+                        btnjogar.onclick = function () { location.reload() };
+                        btnjogar.style.width = '120px';
+                        btnjogar.focus();
+                        return;
+                    }
+
+                    // Reduzindo tentativas
+                    tentativas--;
+                    btnjogar.value = `${contador + 1}º Tentativa`;
+                    contador++;
+                    valortentativa.value = '';
+                    valortentativa.focus();
+                }
+                break;
+        }
+    }
