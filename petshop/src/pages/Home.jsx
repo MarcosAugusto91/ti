a0d472eeb92edb308f7b66a1a5cd5781dcfccac5
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2025-11-11 15:57:46 -0300

    .

--- petshop/src/pages/Home.jsx
+++ petshop/src/pages/Home.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
     return(
-        <h1>Título</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima hic consequuntur dolorem odio? Repellendus modi voluptates optio molestiae animi provident, possimus repudiandae illo, fugiat eaque ad rerum cum asperiores!</p>
+              <main>
+            <div>
+                <h2>Pet notícias</h2>
+            </div>
+            <section>
+                <article>
+                    <h2>Banho no cão</h2>
+                    <p>Banhos regulares mantém os pelos e pele de seu cachorro limpa, renovada e saudável. O processo de ensaboar e escovar remove resíduos de pelos e peles que já estão mortas, providenciando um maior respiro da pele de seu cachorro</p>
+                </article>
+            </section>
+        </main>
     )
 }
 
--- petshop/src/pages/Sobre.jsx
+++ petshop/src/pages/Sobre.jsx
@@ -1,6 +1,15 @@
 const Sobre = () => {
-    return(
-        <></>
+    return (
+        <main>
+            <div>
+                <h2>Sobre Meu amigo cão</h2>
+            </div>
+            <section>
+                <article>
+                    Na Meu amigo cão, o nosso sucesso como uma organização é conduzida pelo cuidado que temos com nossos animais.!!!
+                </article>
+            </section>
+        </main>
     )
 }
 
