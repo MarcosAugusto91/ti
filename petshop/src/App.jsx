c59b318e5354b2a12cdea59a25bf67255a9eb180
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2025-11-11 15:13:44 -0300

    Atualização

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <h1>Hello Worlds</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
