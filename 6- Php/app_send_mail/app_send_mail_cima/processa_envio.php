591c391bfd9187dfd9b98cc3b82f77cdf1e5870b
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-01-21 19:06:28 -0300

    Update processa_envio.php

--- 6- Php/app_send_mail/app_send_mail_cima/processa_envio.php
+++ 6- Php/app_send_mail/app_send_mail_cima/processa_envio.php
@@ -57,7 +57,10 @@ class Mensagem {
 			$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 			$mail->Username   = 'marcosaugusto2410@gmail.com';                     //SMTP username
-			$mail->Password   = 'ajvu pifn guxk ieqh';                               //SMTP password
+			$mail->Password   = 'etlz ohki jzpf hjnp';                             //SMTP password
+            //Para obter esta senha, vá em sua conta google >> Gerenciar sua conta Google >> Segurança e Login >> 
+            //Verificação em duas etapas >> Senhas de App >> coloque o nome do app e criar >> Copie e cole acima a chave disponibilizada.
+
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
