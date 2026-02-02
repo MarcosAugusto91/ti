17da9b57b42f10f4ad934e77e2868b9b717ec969
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-02-02 15:27:41 -0300

    Enviando arquivos Php

---
+++ 6- Php/1- Php_estruturado/0- index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>  
+        <meta charset="utf-8"/>
+    </head> 
+    <body>
+        <?php 
+            echo 'Utilizando a tag padrão'; 
+            $a = 'Imprimindo o valor de uma variável!';
+            echo '<hr>';
+            echo $a;
+            echo '<hr>';
+        ?>
+        <?= 'Utilizando a tag de impressão'; ?>
+    </body>
+</html>
