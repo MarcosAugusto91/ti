9277cbe92b693a78aa2a9d7c32d39a54ca6368ac
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-03-20 17:32:22 -0300

    Routers

---
+++ 8- React/Senac Santos/6- Routers/Routers/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+import './Cabecalho.css'
+
+const Cabecalho = () => {
+  return (
+    <div className='cabecalho'>
+      <h1>Aqui é o nosso Cabeçalho</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
