ff535e79fc50e3c1bddd17699ef806b44654816b
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-03-23 14:19:11 -0300

    Update App.jsx

--- 8- React/Senac Santos/6- Routers/Routers/src/App.jsx
+++ 8- React/Senac Santos/6- Routers/Routers/src/App.jsx
@@ -1,7 +1,5 @@
 import './App.css'
-
 // COMO UTILIZAR O JSON SERVER A TRAZER DADOS DE UMA API:
-
 // 1- npm install json-server@0.17.0
 // 2- Criar o arquivo db.json com os dados que vamos utilizar posteriormente
 // 3- Dentro do arquivo package.json adionar dentro de scripts os seguintes comandos: 
@@ -15,13 +13,11 @@ import './App.css'
 // 4- Executar o comando: npm run server
 // 5- Agora você poderá acessar aos produtos em http://localhost:3000/produtos 
 
-
 // UTILIZANDO ROUTERS:
-
 //0- npm install react-router-dom
 
 //1- config react routers
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 import Cabecalho from './components/Cabecalho'
 
@@ -33,14 +29,19 @@ function App() {
 
   return (
     <>
-      <Cabecalho/>
-      
-      {/* 
-      2- Criando as rotas de acordo com a Url da Página 
-      3- Após colocar os caminhos e posicionar as pages crie uma pasta pages e crie os componentes lá dentro com seus css;
-      */}
-
       <BrowserRouter>
+        <Cabecalho/>
+        
+        {/* 5- Criando botões que redirecionam por meio do Link */}
+        <nav style={{display: 'flex', gap: '10px', padding: '10px'}}>
+          <Link to="/"><button>Home</button></Link>
+          <Link to="/about"><button>About</button></Link>
+        </nav>
+
+        {/* 
+        2- Criando as rotas de acordo com a Url da Página 
+        3- Após colocar os caminhos e posicionar as pages crie uma pasta pages e crie os componentes lá dentro com seus css;
+        */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
