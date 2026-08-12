c05728d9e8770133086c8160b05b24a87dfc1169
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-08-12 17:50:47 -0300

    .

--- 7- React Native/Ex03- Inserindo imagem, stilos e variáveis/App.js
+++ 7- React Native/Ex03- Inserindo imagem, stilos e variáveis/App.js
@@ -1,30 +1,25 @@
 import React, {Component} from 'react';
 import { View, Text, Image } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 class App extends Component{
   render(){
 
     let nome = 'Steve Jobs';
-    let imagem = 'https://sujeitoprogramador.com/steve.png';
+    let imagem = 'https://reactnative.dev/img/tiny_logo.png';
 
     return(
-      <View>
-        <Text style={{marginTop:35}}>
-          Meu primeiro App:
-        </Text>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#000'
+      }}>
 
-        <Text style={{
-          color: 'blue', 
-          fontSize: 20}}>
+        <Text style={{color: 'blue', fontSize: 20}}>
           {nome} {/* Aplicando o valor da minha variável */}
         </Text>
 
         <Image 
           source={{uri: imagem }}
-          style={{width:420, height:200}}> 
+          style={{width: 300, height: 200, resizeMode: 'contain'}}
+        > 
         </Image>
-
       </View>
     );
   }
