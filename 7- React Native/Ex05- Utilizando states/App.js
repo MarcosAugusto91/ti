4d91e02f688b33c770ba10fac2bd8528178b76cd
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-08-17 16:32:14 -0300

    Update App.js

--- 7- React Native/Ex05- Utilizando states/App.js
+++ 7- React Native/Ex05- Utilizando states/App.js
@@ -1,28 +1,29 @@
 import React, {Component} from 'react';
 import { View, Text, Button } from 'react-native';
 
-// Classe App que herda de Component (Component do React nos dá superpoderes!)
+
 class App extends Component{
 
-  // Constructor é executado PRIMEIRO quando o componente nasce
-  // Props é como um "parâmetro" que vem de componentes pai
   constructor(props){
-    
-    // super(props) = "avô, você que é o React.Component, prepare-se!"
-    // Precisa chamar antes de usar this.state e this.props
     super(props);
     
-    // STATE = dados DINÂMICOS do componente (podem mudar)
-    // Quando state muda, a tela renderiza de novo automaticamente
     this.state = {
-      nome: '' // Começa vazio, mas vai receber 'Marcos Augusto' quando clicar
+      nome: '' // Começa vazio, mas vai receber 'nome' quando clicar
     };
 
+    this.entrar = this.entrar.bind(this);
+  }
+
+    // Classe App que herda de Component (Component do React nos dá superpoderes!)
+    // Constructor é executado PRIMEIRO quando o componente nasce
+    // Props é como um "parâmetro" que vem de componentes pai
+    // super(props) = "avô, você que é o React.Component, prepare-se!"
+    // Precisa chamar antes de usar this.state e this.props
+    // STATE = dados DINÂMICOS do componente (podem mudar)
+    // Quando state muda, a tela renderiza de novo automaticamente
     // BIND = dá permissão para a função entrar() acessar 'this' (o próprio componente)
     // Sem bind, quando a função é chamada, 'this' fica undefined
     // Pense como: "entrar, você tem acesso total a tudo do App!"
-    this.entrar = this.entrar.bind(this);
-  }
 
   // Função que muda o STATE
   entrar(nome){
@@ -36,14 +37,14 @@ class App extends Component{
   // render = desenha a tela
   render(){
     return(
-      <View style={{marginTop:40,}}>
+      <View style={{marginTop:50,}}>
         {/* Mostrando o valor do state.nome na tela */}
-        {/* Enquanto não clicar, mostra vazio. Depois de clicar, mostra 'Marcos Augusto' */}
+        {/* Enquanto não clicar, mostra vazio. Depois de clicar, mostra 'nome' */}
         <Text style={{fontSize:23, textAlign:'center'}}>
           {this.state.nome}
         </Text>
         
-        {/* Quando clicar, chama a função entrar() passando 'Marcos Augusto' */}
+        {/* Quando clicar, chama a função entrar() passando 'nome' */}
         {/* A função muda o state, a tela renderiza, e o nome aparece na tela */}
         <Button title='entrar' onPress={() => this.entrar('Marcos Augusto')}></Button>
       </View>
