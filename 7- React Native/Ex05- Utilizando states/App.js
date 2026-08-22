49cc41de253423ff5498c80d1498658829a60ab1
Marcos Augusto <159222225+MarcosAugusto91@users.noreply.github.com>
2026-08-22 17:57:56 -0300

    Update App.js

--- 7- React Native/Ex05- Utilizando states/App.js
+++ 7- React Native/Ex05- Utilizando states/App.js
@@ -4,30 +4,31 @@ import { View, Text, Button } from 'react-native';
 // Classe App que herda de Component (Component do React nos dá superpoderes!)
 class App extends Component{
 
-  // Constructor é executado PRIMEIRO quando o componente nasce
-  // Props é como um "parâmetro" que vem de componentes pai
   constructor(props){
-    
+    super(props);
+    this.state = {
+      nome: '' // Começa vazio, mas vai receber algo quando clicar
+    };
+
+  this.entrar = this.entrar.bind(this);
+    // Constructor é executado PRIMEIRO quando o componente nasce
+    // Props é como um "parâmetro" que vem de componentes pai
+
     // super(props) = "avô, você que é o React.Component, prepare-se!"
     // Precisa chamar antes de usar this.state e this.props
-    super(props);
-    
+
     // STATE = dados DINÂMICOS do componente (podem mudar)
     // Quando state muda, a tela renderiza de novo automaticamente
-    this.state = {
-      nome: '' // Começa vazio, mas vai receber 'Marcos Augusto' quando clicar
-    };
 
     // BIND = dá permissão para a função entrar() acessar 'this' (o próprio componente)
     // Sem bind, quando a função é chamada, 'this' fica undefined
-    // Pense como: "entrar, você tem acesso total a tudo do App!"
-    this.entrar = this.entrar.bind(this);
+    // Pense como: "entrar, você tem acesso total a tudo do App!" 
   }
 
   // Função que muda o STATE
+  // setState = "React, muda meu state e renderiza a tela de novo!"
+  // Por isso a tela atualiza quando o estado muda
   entrar(nome){
-    // setState = "React, muda meu state e renderiza a tela de novo!"
-    // Por isso a tela atualiza quando o estado muda
     this.setState({
       nome: nome
     })
@@ -38,7 +39,7 @@ class App extends Component{
     return(
       <View style={{marginTop:40,}}>
         {/* Mostrando o valor do state.nome na tela */}
-        {/* Enquanto não clicar, mostra vazio. Depois de clicar, mostra 'Marcos Augusto' */}
+        {/* Enquanto não clicar, mostra vazio. Depois de clicar, mostra 'o Nome' */}
         <Text style={{fontSize:23, textAlign:'center'}}>
           {this.state.nome}
         </Text>
